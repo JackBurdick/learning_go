957b241c177ff994e1ce6c936bf9bf9ed3a728f9
Jack Burdick <jburdick2015@fau.edu>
2017-04-30 14:00:01 -0400

    clean print statements

--- my_submission/playground.go
+++ my_submission/playground.go
@@ -8,13 +8,22 @@ import (
 	"path/filepath"
 )
 
+const NUMSTEPS = 16
+
+// RESOURCES:
+// read file examples: https://gobyexample.com/reading-files
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+}
+
 func main() {
 	// get list of file names at target directory
 	inDataDirectory := "fixtures"
 	files, err := ioutil.ReadDir(inDataDirectory)
-	if err != nil {
-		fmt.Println("error: ", err)
-	}
+	checkError(err)
 
 	// clean list names
 	// - remove .DS_Store
@@ -38,83 +47,57 @@ func main() {
 		// open file
 		fullPath := filepath.Join(inDataDirectory, fileName)
 		fileContents, err := ioutil.ReadFile(fullPath)
-		if err != nil {
-			fmt.Println("ERROR")
-		}
+		checkError(err)
 		//fmt.Printf("%s\n", hex.Dump(fileContents))
 		buf := bytes.NewReader(fileContents)
 		fileLen = len(fileContents)
-		//fmt.Printf("Full length: %v\n", len(fileContents))
 
 		// Header: SPLICE
 		err = binary.Read(buf, binary.BigEndian, &spliceHeader)
-		if err != nil {
-			fmt.Println("ERROR")
-		}
-		fmt.Printf("SPLICE: %s\n", spliceHeader)
-		//fmt.Printf("JACK - %v\n", binary.Size(spliceHeader))
+		checkError(err)
 		fileLen -= binary.Size(spliceHeader)
 
 		// Header: track size is big endian
 		err = binary.Read(buf, binary.BigEndian, &trackSize)
-		if err != nil {
-			fmt.Println("ERROR")
-		}
-		fmt.Printf("trackSize: %v\n", trackSize)
+		checkError(err)
 		fileLen -= binary.Size(trackSize)
 
 		// Header: version
 		err = binary.Read(buf, binary.BigEndian, &versionString)
-		if err != nil {
-			fmt.Println("ERROR")
-		}
-		fmt.Printf("VERSION: %s\n", versionString)
+		checkError(err)
 		fileLen -= binary.Size(versionString)
 
 		// Header: tempo
 		// NOTE: tempo is little Endian?
 		err = binary.Read(buf, binary.LittleEndian, &tempo)
-		if err != nil {
-			fmt.Println("ERROR")
-		}
-		fmt.Printf("Tempo: %v\n", tempo)
+		checkError(err)
 		fileLen -= binary.Size(tempo)
-		fmt.Printf("Cur Len -------> %v\n", fileLen)
-		//fmt.Printf("len - %v", len(buf))
-
-		// read file examples: https://gobyexample.com/reading-files
 
 		// Read in body. id+name + 16 steps
-		// while != EOF
 		// TODO: Issue is with pattern 5...
 		for fileLen > 0 {
 			// ID
-			//curIndex
 			err = binary.Read(buf, binary.BigEndian, &id)
-			fmt.Printf("id: %v\n", id)
+			checkError(err)
 			fileLen -= binary.Size(id)
 
 			// Length of instrument name
 			err = binary.Read(buf, binary.BigEndian, &nameLength)
-			fmt.Printf("name length: %v\n", nameLength)
+			checkError(err)
 			fileLen -= binary.Size(nameLength)
 
 			// name of instrument
 			nameBuf := make([]byte, nameLength)
 			err = binary.Read(buf, binary.LittleEndian, &nameBuf)
-			fmt.Printf("name: %s\n", nameBuf)
+			checkError(err)
 			fileLen -= binary.Size(nameBuf)
 
 			// steps
-			// 16 is const
-			stepBuf := make([]byte, 16)
+			stepBuf := make([]byte, NUMSTEPS)
 			err = binary.Read(buf, binary.LittleEndian, &stepBuf)
-			for _, num := range stepBuf {
-				fmt.Printf("%v", num)
-			}
-			fmt.Printf("\nsteps: %v\n", stepBuf)
+			checkError(err)
 			fileLen -= binary.Size(stepBuf)
-			fmt.Printf("JACK ==============: %v\n", fileLen)
+
 		}
 
 	}
