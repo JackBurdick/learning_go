550025fd35a7a0157887b0805d14433591949f50
Jack Burdick <jburdick2015@fau.edu>
2017-05-08 16:42:08 -0400

    refactor into single file

--- drum/decoder.go
+++ drum/decoder.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 // config
@@ -32,6 +33,48 @@ func checkError(err error) {
 	if err != nil {
 		fmt.Println("error: ", err)
 	}
+}
+
+func (curTrack *Pattern) String() string {
+	// create string of specified track information
+	// include: specific track information from struct
+	// loop instruments in the track and print their steps
+
+	// write to buffer then return as buffer.String(), strings are immutable
+	var buffer bytes.Buffer
+	// track header;
+	// Saved with HW Version: 0.909
+	// Tempo: 240
+	cleanedVersionString := fmt.Sprintf("%s", curTrack.versionString)
+	cleanedVersionString = strings.Trim(cleanedVersionString, "\x00")
+	curString := fmt.Sprintf("Saved with HW Version: %s\n", cleanedVersionString)
+	//curString = strings.Trim(cleanedVersionString, "\n")
+
+	buffer.WriteString(curString)
+	//buffer.WriteString(fmt.Sprintf("Saved with HW Version: %s\n", curString))
+	buffer.WriteString(fmt.Sprintf("Tempo: %v\n", curTrack.tempo))
+
+	// print instrument/step info > (99) Maracas	|x-x-|x-x-|x-x-|x-x-|
+	for _, instrument := range curTrack.instruments {
+		// identification > (0) SubKick
+		buffer.WriteString(fmt.Sprintf("(%v) %s\t", instrument.instrumentID, instrument.instrumentName))
+		// steps > |x---|----|x---|----|
+		for i, step := range instrument.steps {
+			if i%4 == 0 {
+				buffer.WriteString("|")
+			}
+			// per spec. exception: print "E" if unknown
+			if step == 1 {
+				buffer.WriteString("x")
+			} else if step == 0 {
+				buffer.WriteString("-")
+			} else {
+				buffer.WriteString("E")
+			}
+		}
+		buffer.WriteString("|\n")
+	}
+	return buffer.String()
 }
 
 func parseTrackToStruct(fileContents []byte) Pattern {
